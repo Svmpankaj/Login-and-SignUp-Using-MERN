ba41e334bac6831cd82a61dea97ca9b1a3ef4cee
Svmpankaj <svmpankaj@gmail.com>
2023-09-22 19:57:46 +0530

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const Home = () => {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+export default Home
