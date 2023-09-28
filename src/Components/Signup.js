1d51de65af1353af5342d92ce7c22f2cce4b1cff
Svmpankaj <svmpankaj@gmail.com>
2023-09-28 23:48:12 +0530

    adding mongodb

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -21,7 +21,7 @@ const Login = () => {
 
     return (
         <div className='login'>
-            <h1>login</h1>
+            <h1>Signup</h1>
             <form action="POST">
                 <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder='Email' name='' />
                 <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder='Password' name='' />
